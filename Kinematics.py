50a1d3d6ff896a6494421fc242f4340f90298e20
Deepavalli <dandudeepa27@gmail.com>
2026-04-03 11:51:26 +0530

    Create Kinematics.py

---
+++ Kinematics.py
@@ -0,0 +1,13 @@
+# SESSION 22 – Kinematics (Differential Drive)
+
+# Step 1: Input wheel speeds
+left = 3
+right = 3
+
+# Step 2: Determine motion
+if left == right:
+    print("Straight Line Motion")
+else:
+    print("Turning Motion")
+
+print("\nProgram Executed Successfully")
